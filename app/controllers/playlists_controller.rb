bfb5170f9b236d59805b9046d9f0f52ae741dab0
MorrisFreeman <morrisfreeman0718@gmail.com>
2018-10-01 14:07:30 +0900

    [add]register tags

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -7,6 +7,7 @@ class PlaylistsController < ApplicationController
 
   def create
     @playlist = current_user.playlists.new(playlist_params)
+    @playlist.tags.build(params[:tag][:content].split(",").map { |e| {content: e} })
     if @playlist.save
       redirect_to root_path, notice: "Added your playlist!"
     else
@@ -19,6 +20,6 @@ class PlaylistsController < ApplicationController
 
   private
     def playlist_params
-      params.require(:playlist).permit(:content, tag: [:content, :playlist_id])
+      params.require(:playlist).permit(:content, tags_attributes: [:content, :playlist_id])
     end
 end
